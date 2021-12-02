f405f0e9b4c5e210146afdb021d0af199caf2071
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-12-02 13:51:56 +0700

    improve code
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- App.js
+++ App.js
@@ -15,196 +15,208 @@ import close from './assets/close.png';
 // Photo
 import photo from './assets/photo.jpg';
 
-export default function SliderMenu(){
+export default function App() {
   const [currentTab, setCurrentTab] = useState("Home");
-  // To get the current status of menu...
+  // To get the curretn Status of menu ...
   const [showMenu, setShowMenu] = useState(false);
 
-  //Animated multiple
+  // Animated Properties...
+
   const offsetValue = useRef(new Animated.Value(0)).current;
   // Scale Intially must be One...
   const scaleValue = useRef(new Animated.Value(1)).current;
   const closeButtonOffset = useRef(new Animated.Value(0)).current;
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{justifyContent: 'flex-start', padding: 15}}>
 
-        <Image source={profile}
-          style={{
-            width: 60,
-            height: 60,
-            borderRadius: 10,
-            marginTop: 8,
-          }} />
+      <View style={{ justifyContent: 'flex-start', padding: 15 }}>
+        <Image source={profile} style={{
+          width: 60,
+          height: 60,
+          borderRadius: 10,
+          marginTop: 8
+        }}></Image>
+
         <Text style={{
-            fontSize: 20,
-            fontWeight: 'bold',
-            color: 'white',
-            marginTop: 20,
-          }}>
-          Hung Phan
-        </Text>
+          fontSize: 20,
+          fontWeight: 'bold',
+          color: 'white',
+          marginTop: 20
+        }}>Jenna Ezarik</Text>
 
         <TouchableOpacity>
-          <Text
-            style={{
-              marginTop: 6,
-              color: 'white',
-            }}>
-            View Profile
-          </Text>
+          <Text style={{
+            marginTop: 6,
+            color: 'white'
+          }}>View Profile</Text>
         </TouchableOpacity>
 
-        <View style={{flexGrow : 1, marginTop:50 }}>
+        <View style={{ flexGrow: 1, marginTop: 50 }}>
           {
-            //Tab bar
+            // Tab Bar Buttons....
           }
+
           {TabButton(currentTab, setCurrentTab, "Home", home)}
           {TabButton(currentTab, setCurrentTab, "Search", search)}
           {TabButton(currentTab, setCurrentTab, "Notifications", notifications)}
           {TabButton(currentTab, setCurrentTab, "Settings", settings)}
+
         </View>
+
         <View>
-        {TabButton(currentTab, setCurrentTab, "Logout", logout)}
+          {TabButton(currentTab, setCurrentTab, "LogOut", logout)}
         </View>
+
       </View>
 
       {
-        //Overlay view
+        // Over lay View...
       }
 
       <Animated.View style={{
-          flexGrow: 1,
-          backgroundColor: 'white',
-          position: 'absolute',
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          paddingHorizontal: 15,
-          paddingVertical: 20,
-          borderRadius : showMenu ? 15 : 0,
-          //Transforming view..
-          transform :[
-            { scale: scaleValue },
-            { translateX: offsetValue }
-          ]
+        flexGrow: 1,
+        backgroundColor: 'white',
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        paddingHorizontal: 15,
+        paddingVertical: 20,
+        borderRadius: showMenu ? 15 : 0,
+        // Transforming View...
+        transform: [
+          { scale: scaleValue },
+          { translateX: offsetValue }
+        ]
+      }}>
+
+        {
+          // Menu Button...
+        }
+
+        <Animated.View style={{
+          transform: [{
+            translateY: closeButtonOffset
+          }]
         }}>
-          <Animated.View>
-          <TouchableOpacity onPress={()=>{
-            //Do actions 
-            //Scaling the view
+          <TouchableOpacity onPress={() => {
+            // Do Actions Here....
+            // Scaling the view...
             Animated.timing(scaleValue, {
               toValue: showMenu ? 1 : 0.88,
               duration: 300,
               useNativeDriver: true
             })
-          .start()
+              .start()
 
-          Animated.timing(offsetValue, {
-            toValue: showMenu ? 1 : 230,
-            duration: 300,
-            useNativeDriver: true
-          })
-        .start()
+            Animated.timing(offsetValue, {
+              // YOur Random Value...
+              toValue: showMenu ? 0 : 230,
+              duration: 300,
+              useNativeDriver: true
+            })
+              .start()
 
-        Animated.timing(closeButtonOffset, {
-          toValue: !showMenu ? -30 : 0,
-          duration: 300,
-          useNativeDriver: true
-        })
-      .start()
+            Animated.timing(closeButtonOffset, {
+              // YOur Random Value...
+              toValue: !showMenu ? -30 : 0,
+              duration: 300,
+              useNativeDriver: true
+            })
+              .start()
 
-          setShowMenu(!showMenu)
-
+            setShowMenu(!showMenu);
           }}>
-          <Image source={showMenu ? close: menu} style={{
-            width:20,
-            height:20,
-            tintColor:'black',
+
+            <Image source={showMenu ? close : menu} style={{
+              width: 20,
+              height: 20,
+              tintColor: 'black',
+              marginTop: 40,
+
+            }}></Image>
+
+          </TouchableOpacity>
+
+          <Text style={{
+            fontSize: 30,
+            fontWeight: 'bold',
+            color: 'black',
+            paddingTop: 20
+          }}>{currentTab}</Text>
+
+          <Image source={photo} style={{
+            width: '100%',
+            height: 300,
+            borderRadius: 15,
+            marginTop: 25
           }}></Image>
 
-         <Text style ={{ 
-           fontSize:30,
-           fontWeight:'bold',
-           color:'black',
-           paddingTop:20,
-         }}>{currentTab}</Text>
+          <Text style={{
+            fontSize: 20,
+            fontWeight: 'bold'
+            , paddingTop: 15,
+            color:'black',
+            paddingBottom: 5
+          }}>Hung Phan</Text>
 
-         <Image source={photo} style={{
-           width:'100%',
-           height:300,
-           borderRadius: 15,
-           marginTop:20
-         }}></Image>
-
-        <Text style ={{ 
-           fontSize:20,
-           fontWeight:'bold',
-           color:'black',
-           paddingBottom: 5,
-           paddingTop: 15
-         }}>Hung Phan</Text>
-
-         <Text style={{ 
-           color:'black',
-         }}>A developer from vietnam</Text>
-        </TouchableOpacity>
-          </Animated.View>
+          <Text style={{
+            color: 'black'
+          }}>A developer Android from VietNam</Text>
         </Animated.View>
-        {
-          //Menu 
-        }
+
+      </Animated.View>
 
     </SafeAreaView>
   );
 }
 
-//For multiple buttons...
+// For multiple Buttons...
 const TabButton = (currentTab, setCurrentTab, title, image) => {
   return (
-    <TouchableOpacity onPress={()=>{
-      if (title == "Logout"){
 
-      } else{
+    <TouchableOpacity onPress={() => {
+      if (title == "LogOut") {
+        // Do your Stuff...
+      } else {
         setCurrentTab(title)
       }
     }}>
       <View style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          paddingVertical: 8,
-          backgroundColor: currentTab == title ? 'white' :'transparent',
-          paddingLeft: 13,
-          paddingRight: 35,
-          borderRadius: 8,
-          marginTop:15
-        }}>
-        <Image source={image}
-          style={{
-            width: 25,
-            height: 25,
-            color : currentTab == title ? '#535091' : 'white',
-          }} />
-        <Text
-          style={{
-            fontSize: 15,
-            fontWeight: 'bold',
-            paddingLeft: 15,
-            color : currentTab == title ? '#535091' : 'white',
-          }}>
-          {title}
-        </Text>
+        flexDirection: "row",
+        alignItems: 'center',
+        paddingVertical: 8,
+        backgroundColor: currentTab == title ? 'white' : 'transparent',
+        paddingLeft: 13,
+        paddingRight: 35,
+        borderRadius: 8,
+        marginTop: 15
+      }}>
+
+        <Image source={image} style={{
+          width: 25, height: 25,
+          tintColor: currentTab == title ? "#5359D1" : "white"
+        }}></Image>
+
+        <Text style={{
+          fontSize: 15,
+          fontWeight: 'bold',
+          paddingLeft: 15,
+          color: currentTab == title ? "#5359D1" : "white"
+        }}>{title}</Text>
+
       </View>
     </TouchableOpacity>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#535901',
+    backgroundColor: '#5359D1',
     alignItems: 'flex-start',
     justifyContent: 'flex-start',
   },
 });
-
